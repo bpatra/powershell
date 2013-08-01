33d30cf336c715a53afcc09766588179115dbbf6
benoit <benoit.patra@gmail.com>
2013-08-01 21:22:41 +0200

    first (bugged) version for aliasing path .exe

---
+++ createAliasForPathEXE.ps1
@@ -0,0 +1,6 @@
+﻿function CreateAliasesForDirectory($path)
+{
+    $result = Get-ChildItem (Join-Path $path "*") -Include *.exe | ForEach-Object { Set-Alias $_.BaseName $_}                                                                                                        
+}
+
+$env:PATH.Split(';')  | Where-Object {Test-Path $_ } | ForEach-Object { CreateAliasesForDirectory $_ }
