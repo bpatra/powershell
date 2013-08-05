2d86a325cdaab75d3da5b046ee24efe620e825b4
Benoît Patra <benoit.patra@gmail.com>
2013-08-05 10:02:12 +0200

    Simplification of Invoke-AliasWithExeInPath + sample profile script

--- sampleProfile.ps1
+++ sampleProfile.ps1
@@ -1,4 +1,7 @@
-﻿function Export-AliasWithEXEInPATH
+
+$aliasesPath = Join-Path -Path ([System.IO.Path]::GetDirectoryName($PROFILE)) -ChildPath "alias.txt"
+
+function Export-AliasWithEXEInPATH
 {
     $aliasDict = @{}
     $existingAlias = Get-Alias | ForEach-Object {$_.Name}
@@ -19,8 +22,17 @@
                                     Set-Alias -Name $alias -Value $for -Verbose -Scope "Global"
                                 }
                             }             
-
     }
-
     Export-Alias $aliasesPath
 }
+
+if(-Not (Test-Path $aliasesPath))
+{
+    Write-Error "Cannot find alias.txt execute function Export-AliasWithEXEInPATH"
+}
+else
+{
+    Import-Alias $aliasesPath -Force
+}
+
+Import-Module "PowerTab" -ArgumentList "C:\Users\bpatr\Documents\WindowsPowerShell\PowerTabConfig.xml"
