e1a7015d318014280174b1a298415736619157af
bpatra <benoit.patra@gmail.com>
2014-10-03 10:45:59 +0200

    powershell profile when using PowerTab

--- Powershell_profile.ps1
+++ Powershell_profile.ps1
@@ -1,26 +1,65 @@
-#powershell profile of bpatr
-$backupDrive = "Z:/"
-$repo = "C:/gitDev4.1"
+
+$aliasesPath = Join-Path -Path ([System.IO.Path]::GetDirectoryName($PROFILE)) -ChildPath "alias.txt"
+ 
+function Export-AliasWithEXEInPATH
+{
+    $aliasDict = @{}
+    $existingAlias = Get-Alias | ForEach-Object {$_.Name}
+    foreach($aliasName in $existingAlias)
+    {
+        $aliasDict.Add($aliasName,"")
+    }
+    $pathDirectories = $env:PATH.Split(';') | Where-Object {($_ -ne "") -and (Test-Path $_ )}
+    foreach($pathDir in $pathDirectories)
+    {
+        $result = Get-ChildItem (Join-Path $pathDir "*") -Include *.exe | 
+            ForEach-Object {
+                                Write-Host "Checking" $_.BaseName "for aliasing"
+                                If(-Not $aliasDict.ContainsKey($_.BaseName))
+                                {
+                                    $alias = $_.BaseName.ToString()
+                                    $for = $_.Name
+                                    Set-Alias -Name $alias -Value $for -Verbose -Scope "Global"
+                                }
+                            }             
+    }
+    Export-Alias $aliasesPath
+}
+ 
+if(-Not (Test-Path $aliasesPath))
+{
+    Write-Error "Cannot find alias.txt execute function Export-AliasWithEXEInPATH"
+}
+else
+{
+    Import-Alias $aliasesPath -Force
+}
+
+
+function GitLog
+{
+     git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n'' %C(white)%s%C(reset) %C(dim white)- %an%C(reset)' --all
+}
 
 #Subsample of the powershell community extensions
 function Invoke-BatchFile
 {
-    param([string]$Path, [string]$Parameters)  
+    param([string]$Path, [string]$Parameters) 
 
-    $tempFile = [IO.Path]::GetTempFileName()  
+    $tempFile = [IO.Path]::GetTempFileName() 
 
-    ## Store the output of cmd.exe.  We also ask cmd.exe to output   
-    ## the environment table after the batch file completes  
-    cmd.exe /c " `"$Path`" $Parameters && set > `"$tempFile`" " 
+    ## Store the output of cmd.exe.  We also ask cmd.exe to output  
+    ## the environment table after the batch file completes 
+    cmd.exe /c " `"$Path`" $Parameters && set > `"$tempFile`" "
 
-    ## Go through the environment variables in the temp file.  
-    ## For each of them, set the variable in our local environment.  
-    Get-Content $tempFile | Foreach-Object {   
-        if ($_ -match "^(.*?)=(.*)$")  
-        { 
-            Set-Content "env:\$($matches[1])" $matches[2]  
-        } 
-    }  
+    ## Go through the environment variables in the temp file. 
+    ## For each of them, set the variable in our local environment. 
+    Get-Content $tempFile | Foreach-Object {  
+        if ($_ -match "^(.*?)=(.*)$") 
+        {
+            Set-Content "env:\$($matches[1])" $matches[2] 
+        }
+    } 
 
     Remove-Item $tempFile
 }
@@ -38,42 +77,26 @@ function Import-VS2010Vars
 function Import-VS2012Vars
 {
     Invoke-BatchFile "${env:VS110COMNTOOLS}..\..\VC\vcvarsall.bat" $vcargs
-	#VCVARS invoke in VS2012 are silent...
+     #VCVARS invoke in VS2012 are silent...
     Write-Host "VS2012 vcvars loaded..."
 }
 
-function Kill-Vs 
+function Import-VS2013Vars
 {
-	Get-Process | Where-Object {$_.ProcessName -eq "devenv"} | ForEach-Object {kill $_.Id}
-}
-
-function Kill-Exlorer 
-{
-	Get-Process | Where-Object {$_.ProcessName -eq "explorer"} | ForEach-Object {kill $_.Id}
-}
-
-function Kill-Cassini 
-{
-	Get-Process | Where-Object {$_.ProcessName -eq "WebDev.WebServer"} | ForEach-Object {kill $_.Id}
+    Invoke-BatchFile "${env:VS120COMNTOOLS}..\..\VC\vcvarsall.bat" $vcargs
+     #VCVARS invoke in VS2012 are silent...
+    Write-Host "VS2013 vcvars loaded..."
 }
 
 
-function Backup-Git
-{
-		
-	$backupGit = (Join-Path -path $backupDrive -childpath .git)
-	$repoGit = (Join-Path -path $repo -childpath .git)
-	
-	if(Test-Path $backupGit){
-		Write-Host "Start removing previous .git repo in:" $backupGit
-		Remove-Item -Recurse -Force $backupGit
-		Write-Host "Removing .git repo finished!"
-	}
-	
-	Write-Host "Start copying .git repo from:" $repoGit "in:" $backupGit
-	Copy-Item $repoGit $backupGit -recurse -Force
-	Write-Host "Copying .git finished."
-	Write-Host "Copying .gitignore"
-	Copy-Item (Join-Path -path $repo -childpath .gitignore) (Join-Path -path $backupDrive -childpath .gitignore)
-	Write-Host "Backup completed"
-}
+#Import-Module PSReadline
+
+<############### Start of PowerTab Initialization Code ########################
+    Added to profile by PowerTab setup for loading of custom tab expansion.
+    Import other modules after this, they may contain PowerTab integration.
+#>
+
+#Import-Module "PowerTab" -ArgumentList "C:\Users\Benoit\Documents\WindowsPowerShell\PowerTabConfig.xml"
+################ End of PowerTab Initialization Code ##########################
+
+
